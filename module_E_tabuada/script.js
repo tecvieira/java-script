e9533551f2ee9f9719ba0ad38245bf70b4bc87a4
Robson Vieira <tecvander.vieira@gmail.com>
2025-06-24 22:22:17 -0300

    correção

--- module_E_tabuada/script.js
+++ module_E_tabuada/script.js
@@ -2,15 +2,15 @@ function tabuada(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
     if(num.value.length == 0){
-        window.alert('Por favor inserir um número')
+        window.alert('Por favor, inserir um número!')
     }else{
-        let n = number(num.value)
+        let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
         while(c <= 10){
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
-            //item.value = `tab ${c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
